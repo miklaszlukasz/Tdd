c453ff48fc0d423864cb0a9bafe6a13ed2ac43b4
Łukasz Miklasz <miklaszlukasz@gmail.com>
2017-03-08 13:24:34 +0100

    Refactor name variables

--- src/main/java/pl/miklaszlukasz/tdd/calculator/impl/DefaultCalculator.java
+++ src/main/java/pl/miklaszlukasz/tdd/calculator/impl/DefaultCalculator.java
@@ -4,14 +4,14 @@ import pl.miklaszlukasz.tdd.calculator.Calculator;
 
 public class DefaultCalculator implements Calculator {
     @Override
-    public double add(double a, double b) {
-        return a + b;
+    public double add(double number1, double number2) {
+        return number1 + number2;
     }
 
     @Override
-    public double divide(double a, double b) {
-        if (b == 0)
-            throw new IllegalArgumentException("Argument 'divisor' is 0");
-        return a/b;
+    public double divide(double number, double divider) {
+        if (divider == 0)
+            throw new IllegalArgumentException("Argument divider is 0");
+        return number/divider;
     }
 }
